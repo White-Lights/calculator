a66cb30aaccfb8d2922cf5c92d1e8302b7efde81
Whitney Bean <143766355+White-Lights@users.noreply.github.com>
2025-02-04 20:43:25 -0600

    Add initial calculator functionality

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
+const numbers = document.querySelectorAll(".numbers");
+const operators = document.querySelectorAll(".operators");
+const prevOp = document.querySelector("#previousOp");
+const currentOp = document.querySelector("#currentOp");
+const calcBtn = document.querySelector(".calculate");
+
+let op = undefined;
+let leftOperand = "";
+let rightOperand = "";
+
+numbers.forEach((e) => e.addEventListener("click", addOperand));
+operators.forEach((e) => e.addEventListener("click", assignOperator));
+calcBtn.addEventListener("click", () => 
+    operate(Number(leftOperand), op, Number(rightOperand)))
+
+function addOperand(event) {
+    if(op === undefined) {
+        if(leftOperand.length < 12) {
+            leftOperand += event.target.dataset.key;
+            currentOp.textContent = numberWithCommas(leftOperand);
+            console.log(leftOperand);
+        }
+    } else {
+        if(rightOperand.length < 12) {
+            rightOperand += event.target.dataset.key;
+            currentOp.textContent = numberWithCommas(rightOperand);
+            console.log(rightOperand);
+        }
+    }
+}
+
+function assignOperator(event) {
+    op = event.target.dataset.key;
+    prevOp.textContent = `${numberWithCommas(leftOperand)} ${op}`
+}
+
 function add(a, b) { return a + b };
 function subtract(a, b) { return a - b };
 function multiply (a, b) { return a * b };
 function divide(a, b) { return a / b };
 
 function operate(a, op, b) {
+    console.log("is it working");
     let answer;
     switch(op) {
         case "+":
@@ -19,5 +56,11 @@ function operate(a, op, b) {
             answer = divide (a, b);
             break;
     }
-    console.log(answer)
+    currentOp.textContent = answer;
+    prevOp.textContent = "- - - - -";
+    return answer;
+}
+
+function numberWithCommas(x) {
+    return x.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
